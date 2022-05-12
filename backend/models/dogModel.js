b61c1f51e3e479541fdab42584446950b1f47e61
imJackym <imjackyrm@gmail.com>
2022-05-13 02:17:55 +0800

    Create Models

---
+++ backend/models/dogModel.js
@@ -0,0 +1,4 @@
+import express from 'express';
+import User from './dogModel.js';
+
+console.log("dogRoutes.js")
---
+++ backend/models/userModel.js
@@ -0,0 +1,4 @@
+import express from 'express';
+import User from './userModel.js';
+
+console.log("userRoutes.js")
