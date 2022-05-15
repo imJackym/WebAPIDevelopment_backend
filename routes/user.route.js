3d216f42974d7aeb3f509593276cbf5156e8c071
imJackym <imjackyrm@gmail.com>
2022-05-15 21:01:23 +0800

    bug fix userRoute

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,43 +1,14 @@
 // Routers <-> Controllers <-> Services <-> Models
 // Routers are url routing
 
-// import { authEmail, generateToken} from '../auth';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser'
+import { login, register } from '../controllers/user.controller.js';
 
-const Koa = require('koa');
-const Router = require('koa-router');
-const router = Router({ prefix: '/api/v1/users' })
-const UserController = require('../controllers/user.controller');
+const router = Router({ prefix: '/api/v1/user' })
 
+// router.get('/', UserController.getAllUser)
+router.post('/login', bodyParser(), login)
+router.post('/register', bodyParser(), register)
 
-router.get('/', UserController.getAllUser)
-router.post('/login', UserController.getUserLogin)
-router.post('/register', UserController.postUserRegister, generateToken())
-
-// export function authEmail() {
-//   return passport.authenticate('email');
-// }
-
-/** After autentication using one of the strategies, generate a JWT token */
-function generateToken() {
-  return async ctx => {
-    console.log('generating token....')
-    console.log(ctx.passport)
-    const { user } = ctx.passport;
-    if (user === false) {
-      ctx.status = 401;
-    } else {
-      const _token = jwt.sign({ id: user }, config.secret);
-      const token = `JWT ${_token}`;
-
-      const currentUser = await User.findOne({ _id: user });
-
-      ctx.status = 200;
-      ctx.body = {
-        token,
-        user: currentUser,
-      };
-    }
-  };
-}
-
-module.exports = router
+export default router
