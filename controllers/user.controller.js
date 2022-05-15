1561a9e6ade34687525a2f93c0c5ae8037df57f1
imJackym <imjackyrm@gmail.com>
2022-05-15 21:02:38 +0800

    bug fix userController

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,91 +1,74 @@
 // Routers <-> Controllers <-> Services <-> Models
 // Controller set up business logic
 
-var UserService = require('../services/user.service');
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import { isAuth, isAdmin, generateToken } from '../utils.js';
 
-exports.getAllUser = async function (content) {
-  console.log("user.controller.js :: getAllUser");
-  var name = "12345647897";
-  let result = await UserService.getAllUser(name);
-
-  console.log("return => result");
-  console.log(result);
-  console.log("--- end of return ---");
-
-  if (result.length) {
-    content.status = 200
-    content.body = result
-  } else {
-    content.status = 404
-    content.body = "Login Fail"
-    console.log("Login Fail")
-  }
+export const getUser = async function (ctx) {
+  console.log("-------------------------------------------")
+  console.log("getUser : ")
+  const users = await User.find({});
+  console.log(users)
+  res.send(users);
 }
 
-exports.getUserLogin = async function (ctx) {
-  console.log("UserController :: getUserLogin")
-  console.log("ctx.param.userName, " + ctx.param.userName);
-  console.log("ctx.param.userPassword, " + ctx.param.userPassword);
+// signin
+export const login = async (ctx) => {
+  let user = await User.findOne({ name: ctx.request.body.username });
+  console.log(user)
 
-  let result = await UserService.getUserLogin(ctx.param.userName, ctx.param.userPassword);
-
-  console.log("return => result");
-  console.log(result);
-  console.log("--- end of return ---");
-
-  if (result.length) {
-    ctx.body = result;
-    console.log("Login Success")
-  } else {
-    cnx.status = 404
-    ctx.body = "Login Fail"
-    console.log("Login Fail")
-  }
-}
-
-exports.postUserRegister = async function (ctx, next) {
-  const { name, id, password } = ctx.request.body;
-  console.log("UserController :: postUserRegister")
-  console.log("ctx.param.userName, " + name);
-  console.log("ctx.param.userId, " + id);
-  console.log("ctx.param.userPassword, " + password);
-
-  if (name && id && password) {
-    let userResult = await UserService.findOne({ id });
-    if (!userResult) {
-      user = new UserService({name, email});
-      user.password = user.generateHash(password);
-      await UserService.createUser();
-
-      ctx.passport = {
-        user: user._id,
+  if (user) {
+    if (bcrypt.compare(user.password, ctx.request.body.password)) {
+      ctx.status = 200
+      ctx.body = {
+        name: user.name,
+        isAdmin: user.isAdmin,
+        token: generateToken(user),
+        status: 200
       };
-
-      console.log(ctx.passport)
-
-      await next();
-
     } else {
-      ctx.status = 400;
-      ctx.body = { status: 'error', message: 'E-mail already registered' };
+      ctx.status = 404
+      ctx.body = { status: "Submit Fail. Please try again later" }
     }
   } else {
-    ctx.status = 400;
-    ctx.body = { status: 'error', message: 'Invalid email or password' };
+    ctx.status = 404
+    ctx.body = "not found"
+  }
+}
+
+// register
+export const register = async (ctx) => {
+  try {
+    let userV = await User.findOne({
+      name: ctx.request.body.name,
+    });
+
+    let isAdmin = false
+
+    if (userV === null) {
+      ctx.status = 200
+      const newUser = new User({
+        fname: ctx.request.body.fname,
+        lname: ctx.request.body.lname,
+        name: ctx.request.body.name,
+        isAdmin: isAdmin,
+        password: bcrypt.hashSync(ctx.request.body.password),
+        status: 200
+      });
+      let user = await newUser.save();
+      ctx.body = {
+        status: "register"
+      };
+    } else {
+      ctx.body = {
+        status: "duplicate"
+      };
+    }
+  } catch (error) {
+    ctx.body = {
+      status: 400
+    };
   }
 
-  // let result = await UserService.postUserRegister(name, email, password)
-
-  // console.log("return => result");
-  // console.log(result);
-  // console.log("--- end of return ---");
-
-  // if (result.length) {
-  //   ctx.body = result;
-  //   console.log("Register Success")
-  // } else {
-  //   cnx.status = 404
-  //   ctx.body = "Register Fail"
-  //   console.log("Register Fail")
-  // }
 }
