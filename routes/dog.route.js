845f28e30b99b971db4cbe2f38d1ed32e7d0aa95
imJackym <imjackyrm@gmail.com>
2022-05-16 02:01:31 +0800

    Update dog.route.js

--- routes/dog.route.js
+++ routes/dog.route.js
@@ -4,14 +4,14 @@
 import bodyParser from 'koa-bodyparser'
 import Router from 'koa-router'
 import { isAuth, isAdmin, generateToken } from '../utils.js';
-import { getAllDog, addNewDog } from '../controllers/dog.controller.js'
+import { getAllDog, addNewDog, getDogById, updateDogById } from '../controllers/dog.controller.js'
 
 const router = Router({ prefix: '/api/v1/dog' })
 
 router.get('/', getAllDog)
 router.post('/', bodyParser(), isAuth, addNewDog)
-// router.get('/:id([0-9]{1,})', DogController.getDogById)
-// router.post('/:id([0-9]{1,})', bodyParser(), DogController.updateDogById)
+router.get('/:id', getDogById)
+router.post('/:id', bodyParser(), isAuth, updateDogById)
 // router.del('/:id([0-9]{1,})', DogController.deleteDogById)
 
 export default router
