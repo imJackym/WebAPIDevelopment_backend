c78f6dc009ce5798e3505665ed1578e8c58ac4dc
imJackym <imjackyrm@gmail.com>
2022-05-18 01:26:04 +0800

    Update userRoute - favorite list

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,11 +3,14 @@
 
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser'
-import { login, register } from '../controllers/user.controller.js';
+import { login, register, favlist, refavlist } from '../controllers/user.controller.js';
+import { isAuth, isAdmin, generateToken } from '../utils.js';
 
 const router = Router({ prefix: '/api/v1/user' })
 
 router.post('/login', bodyParser(), login)
 router.post('/register', bodyParser(), register)
+router.post('/fav/:id', bodyParser(), isAuth, isAdmin, favlist)
+router.post('/refav/:id', bodyParser(), isAuth, isAdmin, refavlist)
 
 export default router
