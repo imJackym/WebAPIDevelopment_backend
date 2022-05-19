d5d7667317de94c8f71089de1eda709c05a0144c
imJackym <imjackyrm@gmail.com>
2022-05-20 02:21:26 +0800

    Create upload.js

---
+++ upload.js
@@ -0,0 +1,12 @@
+import multer from 'koa-multer'
+import path from 'path'
+
+const storage = multer.diskStorage({
+  destination: `public/images/`,
+  filename(ctx, file, cb){
+    const filename = file.originalname.split('.');
+    cb(null, Date.now() + '.' + filename[1])
+  }
+})
+const upload = multer({storage})
+export default upload
