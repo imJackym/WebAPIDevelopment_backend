ab5e5c9933de28e35fe2bac9f5bd065a01efcad4
imJackym <imjackyrm@gmail.com>
2022-05-20 02:22:32 +0800

    Add favorite list

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,14 +3,15 @@
 
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser'
-import { login, register, favlist, refavlist } from '../controllers/user.controller.js';
+import { login, register, getfavlist, favlist, refavlist } from '../controllers/user.controller.js';
 import { isAuth, isAdmin, generateToken } from '../utils.js';
 
 const router = Router({ prefix: '/api/v1/user' })
 
 router.post('/login', bodyParser(), login)
 router.post('/register', bodyParser(), register)
-router.post('/fav/:id', bodyParser(), isAuth, isAdmin, favlist)
-router.post('/refav/:id', bodyParser(), isAuth, isAdmin, refavlist)
+router.post('/favlist', bodyParser(), getfavlist)
+router.post('/fav/:id', bodyParser(), isAuth, favlist)
+router.post('/refav/:id', bodyParser(), isAuth, refavlist)
 
 export default router
