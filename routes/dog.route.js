582ab5db8484ed25df116a3dfbf95aa4e8dde0b6
imJackym <imjackyrm@gmail.com>
2022-05-20 02:25:46 +0800

    Update dog.route.js

--- routes/dog.route.js
+++ routes/dog.route.js
@@ -3,17 +3,21 @@
 
 import bodyParser from 'koa-bodyparser'
 import Router from 'koa-router'
+import upload from '../upload.js'
 import { isAuth, isAdmin, generateToken } from '../utils.js';
-import { getAllDog, addNewDog, getDogById, updateDogById, search, deleteDogById, filter } from '../controllers/dog.controller.js'
+import { getAllDog, addNewDog, getDogById, updateDogById, search, deleteDogById, filter, uploadimg } from '../controllers/dog.controller.js'
 
 const router = Router({ prefix: '/api/v1/dog' })
 
-router.get('/filter/:breed', filter)
 router.get('/', getAllDog)
-router.post('/', bodyParser(), isAuth, addNewDog)
+router.get('/filter/:breed([A-Za-z0-9]{1,})', filter)
+router.get('/search/:name([A-Za-z0-9]{1,})', search)
+
+router.post('/', bodyParser(), isAuth, isAdmin, addNewDog)
+router.post('/upload', isAuth, isAdmin, upload.single('pic'), uploadimg)
+router.post('/delete/:id([A-Za-z0-9]{1,})', bodyParser(), isAuth, isAdmin, deleteDogById)
+
 router.get('/:id([A-Za-z0-9]{1,})', getDogById)
-router.post('/:id([0-9]{1,})', bodyParser(), isAuth, isAdmin, updateDogById)
-router.get('/search/:name', search)
-router.post('/delete/:id([0-9]{1,})', bodyParser(), isAuth, isAdmin, deleteDogById)
+router.post('/:id([A-Za-z0-9]{1,})', bodyParser(), isAuth, isAdmin, updateDogById)
 
 export default router
